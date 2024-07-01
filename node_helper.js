4367491f54f7b609a71b2f29172cdeb32554b77a
Anthony Ritter <aritterdls@gmail.com>
2024-07-01 12:18:35 -0500

    Update node_helper.js
    Changed to use api.catalog.beer instead of old dead api

--- node_helper.js
+++ node_helper.js
@@ -1,45 +1,62 @@
-/* Magic Mirror
- * Module: MMM-Brewdog
- *
- * By jsteel715
- *
- */
 const NodeHelper = require('node_helper');
 const request = require('request');
-const Typed = require('typed.js');
-
-    
-
-
 
 module.exports = NodeHelper.create({
-
     start: function() {
         console.log("Starting node_helper for: " + this.name);
     },
 
-    getBeer: function(url) {
+    getBeers: function(apiKey) {
+        const url = "https://api.catalog.beer/beer";
+        console.log("Fetching beers from API...");
         request({
             url: url,
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': apiKey
+            }
         }, (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                var result = JSON.parse(body);
-	//			console.log(response.statusCode + result); // uncomment to see in terminal
-                    this.sendSocketNotification('BEER_RESULT', result);
-		
+            if (!error && response.statusCode === 200) {
+                const data = JSON.parse(body);
+                console.log("Beers fetched successfully: " + JSON.stringify(data));
+                if (data.data && data.data.length > 0) {
+                    const randomBeer = data.data[Math.floor(Math.random() * data.data.length)];
+                    this.getBeerDetails(randomBeer.id, apiKey);
+                } else {
+                    console.error("No beers available");
+                    this.sendSocketNotification('NO_BEERS_AVAILABLE');
+                }
+            } else {
+                console.error("API call to get beers failed:", error, response.statusCode);
             }
         });
     },
-    
-    
- 
-    
-    
+
+    getBeerDetails: function(beerId, apiKey) {
+        const url = `https://api.catalog.beer/beer/${beerId}`;
+        console.log("Fetching details for beer ID: " + beerId);
+        request({
+            url: url,
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': apiKey
+            }
+        }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                const beerDetails = JSON.parse(body);
+                console.log("Beer details fetched successfully for: " + beerId);
+                this.sendSocketNotification('BEER_DETAILS_RESULT', beerDetails);
+            } else {
+                console.error("API call to get beer details failed:", error, response.statusCode);
+            }
+        });
+    },
 
     socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET_BEER') {
-            this.getBeer(payload);
+        if (notification === 'GET_BEERS') {
+            this.getBeers(payload.apiKey);
         }
     }
 });
